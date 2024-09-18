8ca60d636b44ca36fa47fb98d2afdc6991a400bc
Sabrina <sabis476@gmail.com>
2024-09-18 14:17:16 -0300

    [Inicio do Hello Word Spring]

---
+++ 01-first-spring-app/src/main/java/com/hello_word/first_spring_app/FirstSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.hello_word.first_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringAppApplication.class, args);
+	}
+
+}
